92bfead1efba9f3795c0abd1faecf4449e3e9cde
unknown <sohamvashists8@gmail.com>
2025-04-16 16:10:13 +0530

    Initial commit

--- Frontend/src/pages/AwarenessPage/AwarenessPage.jsx
+++ Frontend/src/pages/AwarenessPage/AwarenessPage.jsx
@@ -6,10 +6,10 @@ import Footer from "../HomePage/Footer";
 export default function Contactus({ darkMode, toggleDarkMode }) {
   return (
     <div className={darkMode ? "dark" : ""}>
-      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+     
       <div>
         <Section1 darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-        <Footer darkMode={darkMode} />
+      
       </div>
     </div>
   );
